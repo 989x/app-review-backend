a885f28ace9ac499b45551abaf7d108c190d5e63
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-03-05 23:50:40 +0700

    upload file 🧙‍♂️

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users");
 const productRoute = require("./routes/products");
 
+const multer = require("multer")
+
 dotenv.config();
 app.use(express.json());
 
@@ -18,6 +20,25 @@ mongoose
     })
     .then(console.log("Connected to MongoDB 🤩"))
     .catch((err) => console.log(err));
+
+// multer
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, "images");
+    }, 
+    filename:(req, file, cb) => {
+        cb(null, "hello.jpeg");
+        // cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+})
+
+// ---
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
